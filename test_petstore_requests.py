9fba571cbb8a10e8819800fc2d0337709ed7a39a
Pankaj Bhamare <pankaj.bhamare@synchronoss.com>
2020-03-02 01:23:33 +0000

    adding comments to the tests

--- test_petstore_requests.py
+++ test_petstore_requests.py
@@ -9,11 +9,13 @@ petID=variables.petID
 petName=variables.petName
 
 
+# Test to create a pet
 def test_create_pet():
     response = petstore_requests.create_pet(petID)
     assert response.status_code == 200
 
 
+#Test to verify the created pet
 def test_verify_created_pet():
     response = petstore_requests.create_pet(petID)
     response_json = json.loads(response.content)
@@ -21,16 +23,19 @@ def test_verify_created_pet():
     assert response_json == request_body
 
 
+# Test to fetch the created pet
 def test_get_pet():
     response=petstore_requests.get_pet(petID)
     assert response.status_code==200
 
 
+# Test to update the pet_name
 def test_update_pet_name():
     response = petstore_requests.update_pet(petID, petName+"update")
     assert response.status_code==200
 
 
+# Test to update and verify the updated pet
 def test_update_and_verify_pet_name():
     response = petstore_requests.update_pet(petID, petName+"update1")
     response_json = json.loads(response.content)
@@ -38,11 +43,13 @@ def test_update_and_verify_pet_name():
     assert response_json==request_body
 
 
+# Test tp delete the pet
 def test_delete_pet():
     response = petstore_requests.delete_pet(petID)
     assert response.status_code==200
 
 
+# Negetive test
 def test_delete_pet_negative():
     response = petstore_requests.delete_pet(petID)
     assert response.status_code == 404
--- variables.py
+++ variables.py
@@ -1,12 +1,18 @@
 import random
+
+
 petID = random.randint(1, 100)
+
 HOST_URL="https://petstore.swagger.io/v2/pet/"
 
 requestHeaders={
     'api-key':'special-key',
     'Content-Type':'application/json'
     }
+
+
 petName="mario"
+
 
 body = {
         "id": petID,
